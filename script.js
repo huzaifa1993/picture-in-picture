f7b1667d899a2428cf989639c59b8b982a583696
Huzaifa yusuf <huothyfayusuf1993@gmail.com>
2023-02-05 19:13:15 +0000

    adding functionality to the page

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+const videoElement = document.getElementsByClassName('video');
+const button = document.getElementById('button');
 
+async function selectMediaStream() {
+    try{
+        const MediaStream = await navigator.mediaDevices.getDisplayMedia();
+        videoElement.srcObject = MediaStream;
+        videoElement.onloadedmetadata = () => {
+            videoElement.play();
+        }
+    }catch(error){
+        console.log('whoop, error here:', error);
+    }
+}
+
+button.addEventListener('click', async () => {
+    button.disabled = true;
+    await videoElement.requestPictureInPicture();
+    button.disabled = false;
+});
+
+selectMediaStream();
